d797305961295efabebff50b6829201ee76db5f0
Gautier Laurent <gautierlaurent47@gmail.com>
2023-02-02 15:18:08 +0100

    ajout media

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,37 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+  const { name, portrait, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    // creation des elements dans le DOM
+    const article = document.createElement("article");
+    const link = document.createElement('a');
+    const img = document.createElement("img");
+    const h2 = document.createElement("h2");
+    const div =document.createElement("div");
+    const locationCity = document.createElement("p");
+    const tagLine = document.createElement("p");
+    const pricePerDay = document.createElement("p");
+    // attribus des elements
+    img.setAttribute("src", picture);
+    link.href="././photographer.html";
+    h2.textContent = name;
+    locationCity.textContent = city + ", " + country;
+    tagLine.textContent = tagline;
+    pricePerDay.textContent = price + "€/jour";
+    // ajout d'elements au parent article
+    article.appendChild(link);
+    article.appendChild(h2);
+    article.appendChild(div)
+     // ajout d'elements au parent link
+    link.appendChild(img); 
+     // ajout d'elements au parent div
+    div.appendChild(locationCity);
+    div.appendChild(tagLine);
+    div.appendChild(pricePerDay);
+    // on return les elements
+    return article;
+  }
+  return { getUserCardDOM };
 }
